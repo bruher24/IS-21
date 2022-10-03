b7afc2b06d102fa4c78daebe7464d49912187931
bruher24 <glushkovd2424@gmail.com>
2022-10-03 20:01:51 +0400

    Added <select> using React.js function-obgject method

--- public/js/App.js
+++ public/js/App.js
@@ -28,17 +28,67 @@ const e = React.createElement;
 
 
 
-  class showText extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { show : false };
-    }
-    render() {
-        
-            return 'Text showed';
-        
-        
-    }
-  }
-  const domContainer = document.querySelector('#root')
-  ReactDOM.render(e(showText), domContainer)
+// class Inputs extends React.Component {
+//   render() {
+
+//     return e(
+//       'select',
+//       {
+//         id: 'select_1'
+//       },
+//       e(
+//         'option',
+//         {
+//           id: 'bin',
+//           value: '2'
+//         },
+//         '2'
+//       ),
+//       e(
+//         'option',
+//         {
+//           id: 'oct',
+//           value: '8'
+//         },
+//         '8'
+//       ),
+//       e(
+//         'option',
+//         {
+//           id: 'dec',
+//           value: '10'
+//         },
+//         '10'
+//       ),
+//       e(
+//         'option',
+//         {
+//           id: 'hex',
+//           value: '16'
+//         },
+//         '16'
+//       ),
+//     );
+//   }
+
+
+// }
+
+function App(){
+  return(
+    <select id="select_1" value="Test" onChange={(e) => onChange(e.target.value)}>
+      {/* <option selected disabled>тест</option> */}
+      <option>2-ичная</option>
+      <option>8-ичная</option>
+      <option>10-ичная</option>
+      <option>16-ичная</option>
+    </select>
+  );
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
